db8b6d66a80a7bf8913ca8807b52ea6d9a797d14
Brandon Corfman <brandon.c.corfman.civ@army.mil>
2021-12-16 10:51:59 -0500

    Initial commit

---
+++ env_setup/setup_conda_env.bat
@@ -0,0 +1,2 @@
+call conda create -y -p %HOMEPATH%\Desktop\pagination_seven\conda_env python==3.9.* pytest
+call conda activate %HOMEPATH%\Desktop\pagination_seven\conda_env
---
+++ env_setup/teardown_conda_env.bat
@@ -0,0 +1,2 @@
+call conda deactivate
+call conda remove --all -y -p %HOMEPATH%\Desktop\pagination_seven\conda_env
